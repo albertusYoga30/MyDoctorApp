e1e0b9ecb00e82c7762423942a556d764c2eeb4a
albertusYoga30 <albertus.yoga3003@gmail.com>
2022-09-01 22:14:03 +0700

    add navigation graph for sign up

--- app/src/main/java/com/example/mydoctor/MainActivity.kt
+++ app/src/main/java/com/example/mydoctor/MainActivity.kt
@@ -3,17 +3,26 @@ package com.example.mydoctor
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import com.example.mydoctor.view.GetStartedActivity
+import com.example.mydoctor.databinding.ActivityMainBinding
+import com.example.mydoctor.view.SignInActivity
+import com.example.mydoctor.view.SignUpActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-//        Handler(Looper.getMainLooper()).postDelayed({
-//            startActivity(Intent(this, GetStartedActivity::class.java))
-//        })
+        binding.btnGetStarted.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        binding.btnSignIn.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/mydoctor/view/SignUpActivity.kt
+++ app/src/main/java/com/example/mydoctor/view/SignUpActivity.kt
@@ -2,11 +2,25 @@ package com.example.mydoctor.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.mydoctor.R
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity(R.layout.activity_sign_up) {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_sign_up) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
